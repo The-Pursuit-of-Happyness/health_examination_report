9a3956fa01c8950c368712db48631df80d59674d
guofen <1779144665@qq.com>
2019-09-23 22:32:48 +0800

    添加图表

--- js/gradeScoreChart.js
+++ js/gradeScoreChart.js
@@ -6,11 +6,9 @@ const option = {
             left:'left',
             data:['2016','2017']
         },
-        width:400,
-        height:400,
         grid:{
-            left:200,
-            top:200,
+            left:100,
+            right:20,
           containLabel:true,
           borderColor:'red'
         },
